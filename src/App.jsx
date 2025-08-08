3701f8ceca162e561d8bac2433fbf68eb52b1f37
TimLym <january26148@gmail.com>
2025-08-08 20:29:34 +0800

    footer 進度1%

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Topbar from './components/Topbar'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
       <Topbar></Topbar>
+      <Footer></Footer>
     </>
   )
 }
