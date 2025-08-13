2ba9da619fda46362afb90726f642e2520ca0b38
zoena5303 <smile1997590@gmail.com>
2025-08-13 16:35:22 +0800

    FQA寫入圖片

--- src/pages/FQA.jsx
+++ src/pages/FQA.jsx
@@ -3,11 +3,15 @@ import faqBG from '../assets/images/FAQ-BG.svg' // 匯入 SVG
 const FQA = () => {
   return (
     <div>
-      <h2>常見問題</h2>
-      <p> </p>
+      <h2>我是常見問題</h2>
      
-
-     
+      {/* 方法 2：背景圖片 */}
+      <div style={{ 
+        width: '300px', 
+        height: '200px', 
+        backgroundImage: `url(${faqBG})`, 
+        backgroundSize: 'cover' 
+      }}></div>
     </div>
   )
 }
