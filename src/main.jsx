e0d245450a8fe7e41340799a13531170334d5345
CatBoss <ktv80607@gmail.com>
2025-08-27 14:24:31 +0800

    加蓋動畫環境

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { HashRouter } from 'react-router-dom'
-
-
+// 添加 polyfill 或降級處理
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
