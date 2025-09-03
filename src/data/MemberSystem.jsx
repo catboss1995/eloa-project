02c10c669a727ce9ca614ff468078c46be897c97
TimLym <january26148@gmail.com>
2025-09-03 21:37:52 +0800

    會員、購物車系統完成- 會員資訊與訂單資訊、結帳頁面可連動

--- src/data/MemberSystem.jsx
+++ src/data/MemberSystem.jsx
@@ -27,6 +27,7 @@ const MemberSystem = {
     const newUser = {
       id: Date.now(),
       ...userData,
+      order: [],
       createdAt: new Date().toISOString()
     };
 
@@ -43,16 +44,24 @@ const MemberSystem = {
     if (!user) {
       throw new Error('電子郵件或密碼錯誤');
     }
-    localStorage.setItem(CURRENT_USER_KEY, JSON.stringify(user));    
+    if (!user.orders) {
+      user.orders = [];
+      const users = MemberSystem.getUsers();
+      const userIndex = users.findIndex(u => u.id === user.id);
+      users[userIndex] = user;
+      MemberSystem.saveUsers(users);
+
+    }
+    localStorage.setItem(CURRENT_USER_KEY, JSON.stringify(user));
     return user;
   },
   // 取得當前登入用戶
-  getCurrentUser: ()=>{
+  getCurrentUser: () => {
     const user = localStorage.getItem(CURRENT_USER_KEY);
-    return user ? JSON.parse(user) : null ;
+    return user ? JSON.parse(user) : null;
   },
   // 登出
-  logout : () => {
+  logout: () => {
     localStorage.removeItem(CURRENT_USER_KEY);
   },
 
@@ -60,7 +69,43 @@ const MemberSystem = {
   emailExists: (email) => {
     const users = MemberSystem.getUsers();
     return users.some(user => user.email === email);
-  }
+  },
+
+  // 新增訂單到用戶中
+  addOrderToUser: (userId, orderData) => {
+    const users = MemberSystem.getUsers();
+    const userIndex = users.findIndex(user => user.id === userId);
+    if (userIndex === -1) {
+      throw new Error("找不到用戶");
+    }
+    // 確保用戶有orders陣列
+    if (!users[userIndex].orders) {
+      users[userIndex].orders = [];
+    }
+
+    // 訂單創建
+    const newOrder = {
+      id: 'A' + Date.now(),
+      ...orderData,
+      orderDate: new Date().toISOString(),
+      status: "訂單確認"
+    };
+    users[userIndex].orders.push(newOrder);
+    MemberSystem.saveUsers(users);
+
+    // 更新用戶資料
+    localStorage.setItem(CURRENT_USER_KEY,JSON.stringify(users[userIndex]));
+    
+    return newOrder;
+
+  },
+
+  //取得用戶訂單
+  getUserOrders: (userId) => {
+    const users = MemberSystem.getUsers();
+    const user = users.find(user=> user.id === userId);
+    return user?.orders || [];
+  } 
 };
 
 export default MemberSystem;
